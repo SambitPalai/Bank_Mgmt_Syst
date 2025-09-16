8162c3aed379996232f609d5a1a035b6fae38d32
Sambit Palai <158765214+SambitPalai@users.noreply.github.com>
2025-09-16 21:18:19 +0530

    Add files via upload
    Working on Bank management system and just created user interface of login and signup page .

---
+++ Bank Management System/src/bank/management/system/Login.java
@@ -0,0 +1,120 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Login extends JFrame implements ActionListener {
+
+//Defining it globally so it can be accessed outside the method.
+    JButton login,signup,clear;
+    JTextField cardTextField;
+    JPasswordField pinTextField;
+//Constructor
+    Login(){
+        setTitle("💲💲 AUTOMATED TELLER MACHINE 💲💲");//5. sets title of the interface.
+
+/* 12. Setting layout to null to use or custom layout i.e.
+       label.setBounds(70,10,100,100); or text.setBounds(90,40,90,90); */
+        setLayout(null);
+
+// For the image "logo.jpg"
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));//6. Gets the image form a specifc address it directly checks the src folder and here object i1 is created
+        Image i2 = i1.getImage().getScaledInstance(90,90, Image.SCALE_DEFAULT); //8. Sets the size of the logo where i2 object is created from i1
+        ImageIcon i3 = new ImageIcon(i2); //9. i2 cant be directly used so object i3 is created from i2
+        JLabel label = new JLabel(i3); //10. here we created a JLabel on which we use to show our object i3 i.e. image "logo.jpg"
+        label.setBounds(370,20,90,90); //11. used to set bounds(location) of label (image)
+        add(label); //7. it helps to place JLabel (label) , on the frame
+
+//13. sets colour in the frame
+        getContentPane().setBackground(Color.WHITE );
+
+//14. With the help of JLabel you can write anything on the frame.
+        JLabel text = new JLabel(" WELCOME TO ATM "); //Places JLabel (text)
+        text.setBounds(270,110,400,40); //sets JLabel (text) location
+        add(text);//places JLabel (text) on the frame
+        text.setFont(new Font("Times New Roman", Font.BOLD, 28));
+
+//15. For Card No.
+        JLabel cardno = new JLabel("Card No. :");
+        cardno.setBounds(260,170,400,40);
+        cardno.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(cardno);
+        /*It can be written as "JTextField cardTextField = new JTextField();"
+   but as it is globally defined therefore we write
+    " cardTextField = new JTextField();" */
+        cardTextField = new JTextField();
+        cardTextField.setBounds(380,180,180,21);
+        cardTextField.setFont(new Font("Aerial",Font.BOLD,14));
+        add(cardTextField);
+
+//16. For PIN
+        JLabel pin = new JLabel("PIN :");
+        pin.setBounds(260,220,400,40);
+        pin.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(pin);
+        pinTextField = new JPasswordField();
+        pinTextField.setBounds(380,230,180,21);
+        add(pinTextField);
+
+//17. For Login
+/*It can be written as "JButton login = new JButton("Login");"
+   but as it is globally defined therefore we write
+    "login = new JButton("Login");" */
+        login = new JButton("Login");
+        login.setBounds(325,280,190,19);
+        login.setBackground(Color.black);
+        login.setForeground(Color.white);
+        login.addActionListener(this);
+        add(login);
+
+//18. For Signup
+        signup = new JButton("Signup");
+        signup.setBounds(345,310,155,19);
+        signup.setBackground(Color.black);
+        signup.setForeground(Color.white);
+        signup.addActionListener(this);
+        add(signup);
+
+//19. For clear
+        clear = new JButton("Clear");
+        clear.setBounds(365,340,115,19);
+        clear.setBackground(Color.black);
+        clear.setForeground(Color.white);
+        clear.addActionListener(this);
+        add(clear);
+
+// Customize Frame
+        setSize(800, 480); //2. sets size of frame
+        setVisible(true); //3. makes the frame visible.
+        setLocation(350,200); //4. sets the location for the frame to open on the display
+    }
+
+/*20. As we have implemented the ActionListener interface -> ("public class Login extends JFrame implements ActionListener")
+    due to the abstract method i.e. declared inside the ActionListener interface and not defined therefore
+    we need to override it .
+>> ("When we have an interface and when we implement that interface
+     we need to override all its methods. ")
+>> ActionEvent class lets us know which button was pressed .
+>> ae is the object of the class ActionEvent. */
+    @Override
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource()==clear){
+            cardTextField.setText("");
+            pinTextField.setText("");
+        } else if (ae.getSource()==login) {
+            
+        } else if (ae.getSource()==signup) {
+            
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        /*1.new object of class Login
+        it calls the constructor --> Login() */
+        new Login();
+
+    }
+}
---
+++ Bank Management System/src/bank/management/system/SignupOne.java
@@ -0,0 +1,168 @@
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*; // it contains the random class "Random ram = new Random();"
+import com.toedter.calendar.JDateChooser;
+
+public class SignupOne extends JFrame {
+
+    JTextField nameTextField,fnameTextField , emailTextField,addressTextField,cityTextField,pinTextField,stateTextField;
+    JRadioButton male ,female ,married ,unmarried, other;
+    JDateChooser dateChooser;
+    JButton next;
+
+    SignupOne() {
+
+/*  Never forget to set the layout of the JLabel to null because
+    in default the format will be center aligned and changes can't be done . */
+        setLayout(null);
+
+        getContentPane().setBackground(Color.WHITE);
+
+// IT IS FOR THE HEADING "APPLICATION FORM NO.( RANDOM NUMBER )"
+        Random ram = new Random();
+/*  (Math.abs(ram.nextLong() % 9000L ) + 1000L) this line generates 4 digit positive random no
+    and to ignore to get random negative we used Math.abs */
+        long random = (Math.abs(ram.nextLong() % 9000L ) + 1000L);
+        JLabel formno = new JLabel("APPLICATION FORM NO. "+random);
+        formno.setFont(new Font ("Raleway",Font.BOLD,38));
+        formno.setBounds(135,20,600,40);
+        add(formno);
+
+//  "PERSONAL DETAILS"
+        JLabel presonaldetails = new JLabel("Page 1: Personal Details");
+        presonaldetails.setFont(new Font ("Raleway",Font.BOLD,20));
+        presonaldetails.setBounds(285,80,400,30);
+        add(presonaldetails);
+
+//  For Name
+        JLabel name = new JLabel("Name : ");
+        name.setBounds(100,140,100,30);
+        name.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(name);
+        nameTextField = new JTextField();
+        nameTextField.setBounds(300,140,300,25);
+        nameTextField.setFont(new Font("Raleway",Font.BOLD,14));
+        add(nameTextField);
+//  For Fathers name
+        JLabel fname = new JLabel("Father's name : ");
+        fname.setBounds(100,190,200,30);
+        fname.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(fname);
+        fnameTextField = new JTextField();
+        fnameTextField.setBounds(300,190,300,25);
+        fnameTextField.setFont(new Font("Raleway",Font.BOLD,14));
+        add(fnameTextField);
+//  For DOB
+        JLabel dob = new JLabel("Date Of Birth : ");
+        dob.setBounds(100,240,200,30);
+        dob.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(dob);
+        dateChooser = new JDateChooser();
+        dateChooser.setBounds(300,240,300,25);
+        dateChooser.setForeground(new Color(0,0,0));
+        add(dateChooser);
+
+//  For Gender
+        JLabel gender = new JLabel("Gender : ");
+        gender.setBounds(100,290,100,30);
+        gender.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(gender);
+        male = new JRadioButton("Male");
+        male.setBounds(300,290,80,25);
+        female = new JRadioButton("Female");
+        female.setBounds(390,290,80,25);
+        // Group is made to choose one option and prevents accidental choosing both options .
+        ButtonGroup genderGroup = new ButtonGroup();
+        genderGroup.add(male);
+        genderGroup.add(female);
+        add(male);
+        add(female);
+
+//  For Email Address
+        JLabel email = new JLabel("Email Address : ");
+        email.setBounds(100,340,200,30);
+        email.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(email);
+        emailTextField = new JTextField();
+        emailTextField.setBounds(300,340,300,25);
+        emailTextField.setFont(new Font("Raleway",Font.BOLD,14));
+        add(emailTextField);
+
+//  For Martial Status
+        JLabel mstat = new JLabel("Martial Status : ");
+        mstat.setBounds(100,390,200,30);
+        mstat.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(mstat);
+        married = new JRadioButton("Married");
+        married.setBounds(300,390,90,25);
+        unmarried = new JRadioButton("Unmarried");
+        unmarried.setBounds(400,390,90,25);
+        other = new JRadioButton("Other");
+        other.setBounds(500,390,90,25);
+        // Group is made to choose one option and prevents accidental choosing both options .
+        ButtonGroup martialstat = new ButtonGroup();
+        martialstat.add(married);
+        martialstat.add(unmarried);
+        martialstat.add(other);
+        add(married);
+        add(unmarried);
+        add(other);
+
+//  For address
+        JLabel address = new JLabel("Adderss : ");
+        address.setBounds(100,440,100,30);
+        address.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(address);
+        addressTextField = new JTextField();
+        addressTextField.setBounds(300,440,300,25);
+        addressTextField.setFont(new Font("Raleway",Font.BOLD,14));
+        add(addressTextField);
+//  For city
+        JLabel city = new JLabel("City : ");
+        city.setBounds(100,490,100,30);
+        city.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(city);
+        cityTextField = new JTextField();
+        cityTextField.setBounds(300,490,300,25);
+        cityTextField.setFont(new Font("Raleway",Font.BOLD,14));
+        add(cityTextField);
+//  For pin code
+        JLabel pin = new JLabel("PIN : ");
+        pin.setBounds(100,540,100,30);
+        pin.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(pin);
+        pinTextField = new JTextField();
+        pinTextField.setBounds(300,540,300,25);
+        pinTextField.setFont(new Font("Raleway",Font.BOLD,14));
+        add(pinTextField);
+//  For State
+        JLabel state = new JLabel("State : ");
+        state.setBounds(100,590,100,30);
+        state.setFont(new Font("Times New Roman", Font.BOLD, 20));
+        add(state);
+        stateTextField = new JTextField();
+        stateTextField.setBounds(300,590,300,25);
+        stateTextField.setFont(new Font("Raleway",Font.BOLD,14));
+        add(stateTextField);
+
+//  For Next
+        next = new JButton("Next");
+        next.setBounds(400,640,80,30);
+        next.setFont(new Font("Raleway",Font.BOLD,14));
+        next.setBackground(Color.BLACK);
+        next.setForeground(Color.WHITE);
+        add(next);
+
+        setSize(850,800);
+        setLocation(350,10);
+        setVisible(true);
+
+    }
+
+
+    public static void main(String[] args) {
+        new SignupOne();
+    }
+}
